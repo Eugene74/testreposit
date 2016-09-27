beff8b8a33bf31b7d08a0677dfe887aa5f3f155c
Eugene74 <ea74@ukr.net>
2016-09-27 21:38:50 +0300

    chages 1

--- Test.java
+++ Test.java
@@ -11,6 +11,7 @@ public class Test extends Super {
     Test() {
         this(1);
         System.out.println("Main() contructor");
+        System.out.println("Main() contructor1");
     }
     Test(int i) {
         System.out.println("Main(int) contructor");
